97d4c4961b8b1c5fe23788022b57a16d3573eca7
Joey Davidson <j.davidson18@gmail.com>
2021-11-27 16:40:51 -0500

    useSelector added, state slice imported into BookResult, initialState rendering to page successfully.

--- client/src/features/book/BookResult.js
+++ client/src/features/book/BookResult.js
@@ -9,9 +9,9 @@ function BookResult() {
     <div className={styles["bookResult"]}>
       <div className={styles["bookResult__header"]}>
         <span className={styles["bookResult__span"]}>
-          <h4>Harry Potter's Book Shelf</h4>
+          <h4>{book.title}</h4>
           <p>
-            Written by <span>Noob MaGee</span>
+            Written by <span>{book.author}</span>
           </p>
         </span>
         <span className={styles["bookResult__spanButton"]}>
@@ -20,24 +20,8 @@ function BookResult() {
         </span>
       </div>
       <div className={styles["bookResult__body"]}>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/91ocU8970hL.jpg"
-          alt=""
-        />
-        <p>
-          Harry Potter is a book about a boy Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Assumenda, libero non ipsam ullam in
-          odit distinctio officia consequuntur commodi cum accusantium excepturi
-          quibusdam inventore nam ad ipsum veritatis tempore maxime. Voluptatem
-          perferendis in deleniti, nemo delectus possimus neque labore velit
-          laudantium fugit consequatur nam minus aperiam harum sapiente nesciunt
-          ullam earum autem sequi iste. Ducimus quisquam quod sit autem deleniti
-          aspernatur vitae expedita dolore laudantium tempore eaque excepturi,
-          explicabo voluptatum omnis odit. Itaque ut optio eaque qui, similique
-          in laboriosam fugiat laudantium? Ex maxime tenetur, omnis sit odit
-          veritatis voluptatem atque aliquam quo recusandae dolorem asperiores
-          autem, illo magnam ad.
-        </p>
+        <img src={book.img} alt="" />
+        <p>{book.description}</p>
       </div>
     </div>
   );
