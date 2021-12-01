92f952b307d3245741960e6f588529fa6e20589d
Joey Davidson <j.davidson18@gmail.com>
2021-11-30 21:22:11 -0500

    redux-persist deleted

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,25 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookReducer from "../features/book/bookSlice";
-import storage from "redux-persist/lib/storage";
-import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
-import thunk from "redux-thunk";
 
-const reducers = combineReducers({
-  book: bookReducer,
+export const store = configureStore({
+  reducer: {
+    book: bookReducer,
+  },
 });
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-
-const store = configureStore({
-  reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== "production",
-  middleware: [thunk],
-});
-
-export default store;
