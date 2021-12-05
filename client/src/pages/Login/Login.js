997e840f99e76944844e09ab3f0fae2920c53987
Joey Davidson <j.davidson18@gmail.com>
2021-12-05 03:00:04 -0700

    left side of login done

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -7,6 +7,7 @@ function Login() {
       <div className={styles["login"]}>
         <div className={styles["login_container"]}>
           <div className={styles["login_image"]}>
+            <img src="/images/gbooks.png" className={styles["gbooksImg"]} />
             <span className={styles["login_image_text"]}>
               <h3>Take a Look...</h3>
               <h4>It's in a book - on google.</h4>
